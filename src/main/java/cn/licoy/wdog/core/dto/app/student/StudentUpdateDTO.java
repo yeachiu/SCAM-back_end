e2de8e236af59c3a43eac48f95f757fa62f4cda3
yeachiu <32219751+yeachiu@users.noreply.github.com>
2019-04-16 10:26:48 +0800

    Update StudentUpdateDTO.java

--- src/main/java/cn/licoy/wdog/core/dto/app/student/StudentUpdateDTO.java
+++ src/main/java/cn/licoy/wdog/core/dto/app/student/StudentUpdateDTO.java
@@ -6,15 +6,24 @@ import lombok.Data;
 @Data
 public class StudentUpdateDTO {
 
-    private String id;  //student id
-    private String realName;    //姓名
-    private String stuNum;      //学号
-
-    private GroupVO groupVO;
+//    private GroupVO groupVO;
 //    private String className;   //班级
 //    private String institute;   //学院
 //    private String profession;  //专业
 //    private Integer period;     //年级
 //    private Integer whatClass;  //班号
+
+    @NotBlank(message = "姓名不能为空")
+    private String realName;	//
+
+    @NotBlank(message = "学号不能为空")
+    private String stuNum;	//
+
+    @NotBlank(message = "请选择一个专业")
+    private String dictId;	//字典表代码，如学院、专业
+
+    private Integer period;	//年级，为null表示为专业分组
+
+    private Integer whatClass;	//
 
 }
