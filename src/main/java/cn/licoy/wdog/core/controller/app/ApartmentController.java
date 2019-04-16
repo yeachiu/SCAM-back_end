a3cfb5f23436269e911889aed88658190eee4a54
yeachiu <32219751+yeachiu@users.noreply.github.com>
2019-04-16 18:17:48 +0800

    Update ApartmentController.java

--- src/main/java/cn/licoy/wdog/core/controller/app/ApartmentController.java
+++ src/main/java/cn/licoy/wdog/core/controller/app/ApartmentController.java
@@ -3,6 +3,8 @@ package cn.licoy.wdog.core.controller.app;
 import cn.licoy.wdog.common.annotation.SysLogs;
 import cn.licoy.wdog.common.bean.ResponseCode;
 import cn.licoy.wdog.common.bean.ResponseResult;
+import cn.licoy.wdog.core.dto.app.apartment.ApartmentDTO;
+import cn.licoy.wdog.core.dto.app.apartment.FindApartmentDTO;
 import cn.licoy.wdog.core.service.app.ApartmentMemberService;
 import cn.licoy.wdog.core.service.app.ApartmentService;
 import cn.licoy.wdog.core.vo.app.MemberVO;
@@ -11,10 +13,8 @@ import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -32,8 +32,24 @@ public class ApartmentController {
     @PostMapping(value = {"/list"})
     @ApiOperation(value = "获取部门列表")
     @SysLogs("获取部门列表")
-    public ResponseResult list(){
-        return ResponseResult.e(ResponseCode.OK,apartmentService.list());
+    public ResponseResult list(FindApartmentDTO findApartmentDTO){
+        return ResponseResult.e(ResponseCode.OK,apartmentService.list(findApartmentDTO));
+    }
+
+    @PostMapping(value = {"/add"})
+    @ApiOperation(value = "添加部门")
+    @SysLogs("添加部门")
+    public ResponseResult add(@RequestBody @Validated @ApiParam ApartmentDTO apartmentDTO){
+        apartmentService.add(apartmentDTO);
+        return ResponseResult.e(ResponseCode.OK);
+    }
+
+    @PostMapping(value = {"/update/{id}"})
+    @ApiOperation(value = "更新部门信息")
+    @SysLogs("更新部门信息")
+    public ResponseResult update(@PathVariable("id") @ApiParam(value = "部门ID") String id,@RequestBody @ApiParam(value = "部门信息") ApartmentDTO apartmentDTO){
+        apartmentService.update(id,apartmentDTO);
+        return ResponseResult.e(ResponseCode.OK);
     }
 
     @PostMapping(value = {"/member/list/{id}"})
@@ -68,5 +84,13 @@ public class ApartmentController {
     public ResponseResult remove(@PathVariable("id") @ApiParam("资源ID") String id){
         apartmentService.remove(id);
         return ResponseResult.e(ResponseCode.OK);
+    }
+
+    @PostMapping(value = {"/get/id/{id}"})
+    @ApiOperation(value = ("部门成员列表"))
+    @SysLogs("部门成员列表")
+    public ResponseResult getById(@PathVariable("id") @ApiParam(value = "部门ID") String id){
+
+        return ResponseResult.e(ResponseCode.OK,apartmentService.getById(id));
     }
 }
