03c7435abbcc8c97815956e507f7533a7b0e77d0
yeachiu <32219751+yeachiu@users.noreply.github.com>
2019-04-16 18:07:09 +0800

    Update ApartmentService.java

--- src/main/java/cn/licoy/wdog/core/service/app/ApartmentService.java
+++ src/main/java/cn/licoy/wdog/core/service/app/ApartmentService.java
@@ -12,12 +12,29 @@ public interface ApartmentService extends IService<Apartment> {
      * 获取分组列表
      * @return
      */
-    List<ApartmentVO> list();
+    Page<ApartmentVO> list(FindApartmentDTO findDTO);
 
+    /**
+     * 添加学生信息
+     * @param dto
+     */
+    void add(ApartmentDTO dto);
+
+    /**
+     * 更新学生信息
+     * @param id
+     * @param dto
+     */
+    void update(String id,ApartmentDTO dto);
     /**
      * 删除部门
      * @param id
      */
     void remove(String id);
-	
+
+    /**
+     * 根据ID获取部门数据
+     */
+    ApartmentVO getById(String id);
 }
+
