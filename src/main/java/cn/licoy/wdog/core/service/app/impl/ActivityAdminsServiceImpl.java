ed15edfe4aa40a8af465d5cf7447fe261f8a53b6
yeachiu <32219751+yeachiu@users.noreply.github.com>
2019-04-17 15:19:15 +0800

    Update ActivityAdminsServiceImpl.java

--- src/main/java/cn/licoy/wdog/core/service/app/impl/ActivityAdminsServiceImpl.java
+++ src/main/java/cn/licoy/wdog/core/service/app/impl/ActivityAdminsServiceImpl.java
@@ -3,12 +3,38 @@ package cn.licoy.wdog.core.service.app.impl;
 import cn.licoy.wdog.core.entity.app.ActivityAdmins;
 import cn.licoy.wdog.core.mapper.app.ActivityAdminsMapper;
 import cn.licoy.wdog.core.service.app.ActivityAdminsService;
+import cn.licoy.wdog.core.service.app.ActivityService;
+import cn.licoy.wdog.core.vo.app.ActivityAbstractVO;
+import com.baomidou.mybatisplus.mapper.EntityWrapper;
 import com.baomidou.mybatisplus.service.impl.ServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @Transactional
 public class ActivityAdminsServiceImpl extends ServiceImpl<ActivityAdminsMapper,ActivityAdmins> implements ActivityAdminsService{
-	
+
+    @Autowired
+    private ActivityService activityService;
+
+    @Override
+    public List<ActivityAbstractVO> findActiAbstractByAdminId(String uid) {
+        List<ActivityAbstractVO> activityAbstractVOList = new ArrayList<>();
+        List<ActivityAdmins> activityAdminses = this.selectList(new EntityWrapper<ActivityAdmins>()
+                .eq("user_id",uid));
+        if (activityAdminses != null && activityAdminses.size()>0){
+            for (ActivityAdmins actiAdmin: activityAdminses) {
+                ActivityAbstractVO abstractVO = activityService.getAbstractById(actiAdmin.getActivityId());
+                if (abstractVO != null){
+                    activityAbstractVOList.add(abstractVO);
+                }
+            }
+        }
+        return activityAbstractVOList;
+    }
 }
+
