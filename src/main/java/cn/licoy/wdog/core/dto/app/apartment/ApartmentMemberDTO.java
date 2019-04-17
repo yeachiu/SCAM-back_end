b2a7d50b36696888eb1b3890ce8c66274bc6a96d
yeachiu <32219751+yeachiu@users.noreply.github.com>
2019-04-17 15:35:09 +0800

    Create ApartmentMemberDTO.java

---
+++ src/main/java/cn/licoy/wdog/core/dto/app/apartment/ApartmentMemberDTO.java
@@ -0,0 +1,13 @@
+package cn.licoy.wdog.core.dto.app.apartment;
+
+import lombok.Data;
+
+
+@Data
+public class ApartmentMemberDTO {
+
+    private String aparId;	//
+
+    private String uid;	//
+
+}
