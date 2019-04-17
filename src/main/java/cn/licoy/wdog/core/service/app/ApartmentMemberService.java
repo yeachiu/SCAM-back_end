2f6f0e4d98faa102fb5d4a44d22ac7b88dcb3d97
yeachiu <32219751+yeachiu@users.noreply.github.com>
2019-04-17 15:15:17 +0800

    Update ApartmentMemberService.java

--- src/main/java/cn/licoy/wdog/core/service/app/ApartmentMemberService.java
+++ src/main/java/cn/licoy/wdog/core/service/app/ApartmentMemberService.java
@@ -3,6 +3,7 @@ package cn.licoy.wdog.core.service.app;
 import cn.licoy.wdog.core.entity.app.ApartmentMember;
 import com.baomidou.mybatisplus.service.IService;
 
-public interface ApartmentMemberService extends IService<ApartmentMember> {
-	
+public interface ApartmentMemberService extends IService<ApartmentMember>,
+        BaseService<ApartmentMemberVO,ApartmentMemberDTO,ApartmentMemberDTO,String,FindMemberDTO>{
+
 }
