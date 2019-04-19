e13f0bc8ba6b3fa6d9958b797996eaf84aeda117
yeachiu <32219751+yeachiu@users.noreply.github.com>
2019-04-19 18:10:39 +0800

    Update SysUserMapper.java

--- src/main/java/cn/licoy/wdog/core/mapper/system/SysUserMapper.java
+++ src/main/java/cn/licoy/wdog/core/mapper/system/SysUserMapper.java
@@ -1,12 +1,22 @@
 package cn.licoy.wdog.core.mapper.system;
 
 import cn.licoy.wdog.core.entity.system.SysUser;
+import cn.licoy.wdog.core.vo.system.SysUserVO;
 import com.baomidou.mybatisplus.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
 
 @Mapper
 @Repository
 public interface SysUserMapper extends BaseMapper<SysUser> {
+
+    @Select("SELECT m.apar_id FROM sys_user u, sys_user_auth a, apartment_member m " +
+            "WHERE u.id = a.uid AND a.stu_id = m.uid AND u.id = #{id}")
+    String getAparIdById(String id);
+
+    @Select("SELECT a.stu_id FROM sys_user u, sys_user_auth a " +
+            "WHERE u.id = a.uid AND a.uid = #{id}")
+    String getStuIdById(String id);
 
 }
