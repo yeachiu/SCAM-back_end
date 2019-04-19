ac0a42fcac9ea1eeba3b05e5e12d93e35854ce91
yeachiu <32219751+yeachiu@users.noreply.github.com>
2019-04-19 17:50:06 +0800

    Update SysUserAuthMapper.java

--- src/main/java/cn/licoy/wdog/core/mapper/system/SysUserAuthMapper.java
+++ src/main/java/cn/licoy/wdog/core/mapper/system/SysUserAuthMapper.java
@@ -20,7 +20,12 @@ public interface SysUserAuthMapper extends BaseMapper<SysUserAuth> {
 
     @Select("SELECT a.id, a.uid, a.stu_id, a.real_name, a.stu_num, a.dict_id, a.period, a.what_class, a.`status`, g.`name` AS className " +
             "FROM sys_user_auth a, a_group g, student s " +
-            "WHERE a.stu_id = s.id AND s.group_id = g.id AND a.`status` = 2 AND a.stu_id NOT IN " +
+            "WHERE a.stu_id = s.id AND s.group_id = g.id AND a.`status` = 2 AND a.id NOT IN " +
             "(SELECT m.uid FROM apartment_member m WHERE m.isadmin = 0)")
     List<UserAuthVO> alreadyListExAdmin();
+
+    @Select("SELECT a.id, a.uid, a.stu_id, a.real_name, a.stu_num, a.dict_id, a.period, a.what_class, a.`status`, g.`name` AS className " +
+            "FROM sys_user_auth a, a_group g, student s " +
+            "WHERE a.stu_id = s.id AND s.group_id = g.id AND a.id = #{id}")
+    UserAuthVO getById(String id);
 }
