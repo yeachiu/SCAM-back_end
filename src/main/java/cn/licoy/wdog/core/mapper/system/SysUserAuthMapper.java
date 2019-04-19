06ae95b4c7232c92225fbae57fd062410a293a68
yeachiu <32219751+yeachiu@users.noreply.github.com>
2019-04-19 15:30:41 +0800

    Update SysUserAuthMapper.java

--- src/main/java/cn/licoy/wdog/core/mapper/system/SysUserAuthMapper.java
+++ src/main/java/cn/licoy/wdog/core/mapper/system/SysUserAuthMapper.java
@@ -15,11 +15,12 @@ public interface SysUserAuthMapper extends BaseMapper<SysUserAuth> {
 
     @Select("SELECT a.id, a.uid, a.stu_id, a.real_name, a.stu_num, a.dict_id, a.period, a.what_class, a.`status`, g.`name` AS className " +
             "FROM sys_user_auth a, a_group g, student s " +
-            "WHERE a.stu_id = s.id AND s.group_id = g.id")
+            "WHERE a.stu_id = s.id AND s.group_id = g.id AND a.`status` = 2")
     List<UserAuthVO> alreadyList();
 
-    @Select("SELECT a.id, a.uid, a.stu_id, a.real_name, a.stu_num, a.dict_id, a.period, a.what_class, a.`status`, g.`name` " +
-            "FROM sys_user_auth a, a_group g, student s, apartment_member m " +
-            "WHERE a.stu_id = s.id AND s.group_id = g.id AND a.stu_id = m.uid AND m.isadmin = 1")
+    @Select("SELECT a.id, a.uid, a.stu_id, a.real_name, a.stu_num, a.dict_id, a.period, a.what_class, a.`status`, g.`name` AS className " +
+            "FROM sys_user_auth a, a_group g, student s " +
+            "WHERE a.stu_id = s.id AND s.group_id = g.id AND a.`status` = 2 AND a.stu_id NOT IN " +
+            "(SELECT m.uid FROM apartment_member m WHERE m.isadmin = 0)")
     List<UserAuthVO> alreadyListExAdmin();
 }
