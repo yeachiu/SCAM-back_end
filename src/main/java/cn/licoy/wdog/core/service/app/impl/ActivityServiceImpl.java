b09cf8e50c46fa9d4bf83b70b8841f1a89b076ff
yeachiu <32219751+yeachiu@users.noreply.github.com>
2019-04-22 17:40:45 +0800

    Update ActivityServiceImpl.java

--- src/main/java/cn/licoy/wdog/core/service/app/impl/ActivityServiceImpl.java
+++ src/main/java/cn/licoy/wdog/core/service/app/impl/ActivityServiceImpl.java
@@ -4,14 +4,20 @@ import cn.licoy.wdog.common.exception.RequestException;
 import cn.licoy.wdog.core.dto.app.activity.ActivityAddDTO;
 import cn.licoy.wdog.core.dto.app.activity.ActivityUpdateDTO;
 import cn.licoy.wdog.core.entity.app.*;
-import cn.licoy.wdog.core.entity.system.SysUser;
+import cn.licoy.wdog.core.entity.system.*;
+import cn.licoy.wdog.core.entity.system.SysUserAuth;
 import cn.licoy.wdog.core.mapper.app.ActivityMapper;
 import cn.licoy.wdog.core.service.app.*;
+import cn.licoy.wdog.core.service.system.SysUserAuthService;
 import cn.licoy.wdog.core.service.system.SysUserService;
 import cn.licoy.wdog.core.vo.app.ActivityAbstractVO;
 import cn.licoy.wdog.core.vo.app.ActivityVO;
 import cn.licoy.wdog.core.dto.app.activity.FindActivityDTO;
 import cn.licoy.wdog.core.dto.app.activity.StatusChangeDTO;
+import cn.licoy.wdog.core.vo.app.SimpleGroupVO;
+import cn.licoy.wdog.core.vo.system.SimpleUserAuthVO;
+import cn.licoy.wdog.core.vo.system.SimpleUserVO;
+import cn.licoy.wdog.core.vo.system.SysUserVO;
 import com.alibaba.fastjson.JSONArray;
 import com.baomidou.mybatisplus.enums.IdType;
 import com.baomidou.mybatisplus.mapper.EntityWrapper;
@@ -34,6 +40,8 @@ public class ActivityServiceImpl extends ServiceImpl<ActivityMapper,Activity> im
     @Autowired
     private SysUserService userService;
     @Autowired
+    private SysUserAuthService userAuthService;
+    @Autowired
     private ActivityAdminsService adminsService;
     @Autowired
     private AGroupService groupService;
@@ -43,9 +51,40 @@ public class ActivityServiceImpl extends ServiceImpl<ActivityMapper,Activity> im
     private ActivityFormService formService;
     @Autowired
     private ScoreSettingService scoreService;
+    @Autowired
+    private ApartmentService apartmentService;
+    @Autowired
+    private ActivityMapper mapper;
 
     /**
-     * 活动列表
+     * 某部门活动列表(分页)
+     * @param findDTO 过滤条件
+     * @return
+     * #过滤取消状态#
+     */
+    @Override
+    public Page<ActivityVO> getAllActivityByAparId(FindActivityDTO findDTO) {
+        //校验
+        Boolean exist = apartmentService.existApartment(findDTO.getId());
+        if(!exist) throw RequestException.fail(String.format("数据获取失败，不存在ID为%s的部门",findDTO.getId()));
+        List<ActivityVO> activities = this.mapper.findActivitiesExCancelByAparId(findDTO.getId());
+        for(ActivityVO acti : activities){
+            List<SimpleGroupVO> limitList = limitService.findLimitByActiId(findDTO.getId());
+            if (limitList != null && limitList.size()>0){
+                acti.setGrouplimit(limitList);
+            }
+            List<SimpleUserVO> adminList = userService.findAllSimpleVOByActiId(findDTO.getId());
+            if (adminList != null && adminList.size() > 0){
+                acti.setOtherAdmin(adminList);
+            }
+        }
+        Page<ActivityVO> activitiesPage = new Page<>(findDTO.getPage(),findDTO.getPageSize());
+        activitiesPage.setRecords(activities);
+        return activitiesPage;
+    }
+
+    /**
+     * 活动列表(分页)
      * @param findActivityDTO 过滤条件
      * @return
      * #过滤取消状态#
@@ -64,60 +103,61 @@ public class ActivityServiceImpl extends ServiceImpl<ActivityMapper,Activity> im
     public Page<ActivityVO> getCancelActivityBySplitPage(FindActivityDTO findActivityDTO){
         return null;
     }
-    
-    /**============================================= >>>> addDTO:
-     * ActivityAddDTO(
-     *  title=信息, description=信息系, pictureUrl=/img_20190409010927.jpg,
-     *  signUpTime=Tue Apr 09 00:00:00 CST 2019, deadlineTime=Wed Apr 10 00:00:00 CST 2019, startTime=Tue Apr 16 00:00:00 CST 2019, endTime=Wed Apr 17 00:00:00 CST 2019,
-     *  limitQuota=100, isBlackList=null, isReview=null, status=0,
-     *  groupId=parent 1-1,leaf 1-1-1,leaf 1-1-2,group01,
-     *  rules=[
-     *      {"type":"hidden","field":"activityId","value":"","validate":[],"event":{},"col":{"span":24},"emit":[],"props":{"hidden":false,"visibility":false,"elementId":"fc_12"},"on":{},"options":[],"title":"","className":"","emitEvent":{}},
-     *      {"type":"input","title":"备注","field":"remarks","value":"","props":{"placeholder":"此处可输入备注","hidden":false,"visibility":false,"elementId":"fc_19"},"validate":[],"event":{},"col":{"span":24},"emit":[],"on":{},"options":[],"className":"","emitEvent":{}}
-     *  ],
-     *  scoreData=[{"awardName":"参与成功","awardNum":"","awardScore":2}])
-     **/
+
+    /**
+     * 添加活动
+     * @param addDTO
+     */
     @Override
     @Transactional
     public void add(ActivityAddDTO addDTO) {
         Activity findActivity = this.selectOne(new EntityWrapper<Activity>().eq("title",addDTO.getTitle()));
         if(findActivity != null){
             throw RequestException.fail(
-                    String.format("已经存在用户名为 %s 的用户",addDTO.getTitle()));
+                    String.format("已经存在名称为 %s 的活动",addDTO.getTitle()));
         }
+        SysUserVO currentUser = userService.getCurrentUser();
         findActivity = new Activity();
         BeanUtils.copyProperties(addDTO,findActivity);
         findActivity.setCreateTime(new Date());
-        findActivity.setCreateUser(userService.getCurrentUser().getId());
+        findActivity.setCreateUser(currentUser.getId());
         System.out.println("======================= findActivity:" + findActivity.toString());
         boolean isSuccess = this.insert(findActivity);
         if (!isSuccess){
             throw RequestException.fail("活动创建失败");
         }
         String actiId = findActivity.getId();
-        //  其他管理员   ==>     t_activityAdmins {id,activityId,userId}
+
+        /** 活动管理员 **/
+        //  1. 添加其他管理员
+        List<ActivityAdmins> adminsList = new ArrayList<>();
         if (!addDTO.getOtherAdmin().equals("")){
             //字符串转数组
             String[] adminIds = addDTO.getOtherAdmin().split(",");
-            List<ActivityAdmins> adminsList = new ArrayList<>();
-            ActivityAdmins admin = new ActivityAdmins();
             SysUser user = null;
             for (String uid:adminIds) {
                 user = userService.selectById(uid);
                 if (user == null) {
-                    throw RequestException.fail(
-                            String.format("操作异常：id为 %s 的用户不存在", uid));
+                    throw RequestException.fail(String.format("操作异常：id为 %s 的认证用户不存在", uid));
                 }
+                ActivityAdmins admin = new ActivityAdmins();
                 admin.setActivityId(actiId);
                 admin.setUserId(uid);
                 admin.setCreateTime(new Date());
-                admin.setCreateUser(userService.getCurrentUser().getId());
+                admin.setCreateUser(currentUser.getId());
                 adminsList.add(admin);
             }
-            adminsService.insertBatch(adminsList);
-
         }
-        //  分组限制  ==>   t_groupLimit {id,actiId,groupId}
+        // 2. 添加系统管理员
+        ActivityAdmins admin = new ActivityAdmins();
+        admin.setActivityId(actiId);
+        admin.setUserId(currentUser.getId());
+        admin.setCreateTime(new Date());
+        admin.setCreateUser(currentUser.getId());
+        adminsList.add(admin);
+        adminsService.insertBatch(adminsList);
+
+        /** 分组限制 **/
         if (!addDTO.getGroupId().equals("")){
             String[] groups = addDTO.getGroupId().split(",");
             List<ActivityLimit> limitList = new ArrayList<>();
@@ -138,20 +178,29 @@ public class ActivityServiceImpl extends ServiceImpl<ActivityMapper,Activity> im
             }
             limitService.insertBatch(limitList);
         }
-        //  表单规则 ==>   t_activityForm {id,activityId,rules,status}
+        /** 表单规则 **/
         if (!addDTO.getRules().equals("")){
             ActivityForm actiForm = new ActivityForm();
             actiForm.setActivityId(actiId);
             actiForm.setRules(addDTO.getRules());
+            actiForm.setCreateTime(new Date());
+            actiForm.setCreateUser(userService.getCurrentUser().getId());
 //            actiForm.setStatus();
             formService.insert(actiForm);
         }
-        //    ==>   t_scoreSetting {id,aid,awardName,awardNum,awardScore}
+
+        /** 学分设置 **/
+
         //1.字符串数组转json对象数组
         if (!addDTO.getScoreData().equals("")){
             List<ScoreSetting> scoreList = JSONArray.parseArray(addDTO.getScoreData(),ScoreSetting.class);
             if (scoreList == null && scoreList.size()< 1){
                 throw RequestException.fail("活动学分数据异常");
+            }
+            for (ScoreSetting score : scoreList) {
+                score.setAid(actiId);
+                score.setCreateTime(new Date());
+                score.setCreateUser(userService.getCurrentUser().getId());
             }
             scoreService.insertBatch(scoreList);
 
