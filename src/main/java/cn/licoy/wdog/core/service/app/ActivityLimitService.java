87322f25afe963fe257153871bbc1880caec01df
yeachiu <32219751+yeachiu@users.noreply.github.com>
2019-04-22 16:20:19 +0800

    Update ActivityLimitService.java

--- src/main/java/cn/licoy/wdog/core/service/app/ActivityLimitService.java
+++ src/main/java/cn/licoy/wdog/core/service/app/ActivityLimitService.java
@@ -1,8 +1,19 @@
 package cn.licoy.wdog.core.service.app;
 
 import cn.licoy.wdog.core.entity.app.ActivityLimit;
+import cn.licoy.wdog.core.vo.app.SimpleGroupVO;
 import com.baomidou.mybatisplus.service.IService;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 public interface ActivityLimitService extends IService<ActivityLimit> {
+
+    /**
+     * 活动分组列表
+     * @param actiId
+     * @return
+     */
+    List<SimpleGroupVO> findLimitByActiId(String actiId);
 	
 }
