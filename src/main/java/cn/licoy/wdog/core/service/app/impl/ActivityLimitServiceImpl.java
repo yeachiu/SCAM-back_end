73115414d882c5e39b50acee5c603701fb50f73d
yeachiu <32219751+yeachiu@users.noreply.github.com>
2019-04-22 17:16:54 +0800

    Update ActivityLimitServiceImpl.java

--- src/main/java/cn/licoy/wdog/core/service/app/impl/ActivityLimitServiceImpl.java
+++ src/main/java/cn/licoy/wdog/core/service/app/impl/ActivityLimitServiceImpl.java
@@ -3,12 +3,22 @@ package cn.licoy.wdog.core.service.app.impl;
 import cn.licoy.wdog.core.entity.app.ActivityLimit;
 import cn.licoy.wdog.core.mapper.app.ActivityLimitMapper;
 import cn.licoy.wdog.core.service.app.ActivityLimitService;
+import cn.licoy.wdog.core.vo.app.SimpleGroupVO;
 import com.baomidou.mybatisplus.service.impl.ServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @Transactional
 public class ActivityLimitServiceImpl extends ServiceImpl<ActivityLimitMapper,ActivityLimit> implements ActivityLimitService{
-	
+
+    @Autowired
+    private ActivityLimitMapper mapper;
+    @Override
+    public List<SimpleGroupVO> findLimitByActiId(String actiId) {
+        return this.mapper.findLimitByActiId(actiId);
+    }
 }
