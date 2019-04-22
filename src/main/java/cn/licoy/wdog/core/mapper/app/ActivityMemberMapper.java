9a16054bbe9682c931ddf12a406a81b0a8e59f7c
yeachiu <32219751+yeachiu@users.noreply.github.com>
2019-04-22 11:35:45 +0800

    Update ActivityMemberMapper.java

--- src/main/java/cn/licoy/wdog/core/mapper/app/ActivityMemberMapper.java
+++ src/main/java/cn/licoy/wdog/core/mapper/app/ActivityMemberMapper.java
@@ -1,12 +1,20 @@
 package cn.licoy.wdog.core.mapper.app;
 
-import cn.licoy.wdog.core.entity.app.ActivityMember;
+import cn.licoy.wdog.core.entity.app.ApartmentMember;
+import cn.licoy.wdog.core.vo.app.MemberVO;
 import com.baomidou.mybatisplus.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Mapper
 @Repository
-public interface ActivityMemberMapper extends BaseMapper<ActivityMember>  {
-	
+public interface ApartmentMemberMapper extends BaseMapper<ApartmentMember>  {
+
+    @Select("SELECT a.id, a.real_name AS realName FROM apartment_member m, sys_user_auth a WHERE m.uid = a.id AND m.apar_id = #{aparId}")
+    List<MemberVO> listExcludeAdmin(String aparId);
+
+
 }
