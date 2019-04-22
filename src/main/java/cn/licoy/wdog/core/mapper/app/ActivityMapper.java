7b0f922bec36293f4e240cc68bdd7299bfb56aaf
yeachiu <32219751+yeachiu@users.noreply.github.com>
2019-04-22 17:09:47 +0800

    Update ActivityMapper.java

--- src/main/java/cn/licoy/wdog/core/mapper/app/ActivityMapper.java
+++ src/main/java/cn/licoy/wdog/core/mapper/app/ActivityMapper.java
@@ -9,4 +9,9 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ActivityMapper extends BaseMapper<Activity>  {
 	
+	@Select("SELECT a.id, a.title, a.description, a.picture_url AS pictureUrl, a.signup_time AS signupTime, a.deadline_time AS deadlineTime, a.start_time AS startTime, a.end_time AS endTime, a.`status`, " +
+	    "a.create_time AS createTime, a.create_user AS createUser, a.limit_quota, a.isreview " +
+	    "FROM activity a WHERE a.`status` != 0 AND a.organizer_id = #{aparId}")
+    List<ActivityVO> findActivitiesExCancelByAparId(String aparId);
+	
 }
