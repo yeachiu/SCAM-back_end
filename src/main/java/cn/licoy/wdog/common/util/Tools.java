0854dcdff98fc4e238926e2f79ecaa6a0f32fb07
yeachiu <32219751+yeachiu@users.noreply.github.com>
2019-04-24 18:25:49 +0800

    Update Tools.java

--- src/main/java/cn/licoy/wdog/common/util/Tools.java
+++ src/main/java/cn/licoy/wdog/common/util/Tools.java
@@ -11,6 +11,8 @@ import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.ServletRequest;
 import javax.servlet.http.HttpServletRequest;
+import java.io.*;
+import java.util.List;
 
 /**
  * @author Licoy
@@ -84,6 +86,21 @@ public class Tools {
         if(!b){
             throw RequestException.fail("身份已过期或无效，请重新认证");
         }
+    }
+
+    /**
+     * 集合深拷贝
+     */
+    public static <T> List<T> deepCopy(List<T> src) throws IOException, ClassNotFoundException {
+        ByteArrayOutputStream byteOut = new ByteArrayOutputStream();
+        ObjectOutputStream out = new ObjectOutputStream(byteOut);
+        out.writeObject(src);
+
+        ByteArrayInputStream byteIn = new ByteArrayInputStream(byteOut.toByteArray());
+        ObjectInputStream in = new ObjectInputStream(byteIn);
+        @SuppressWarnings("unchecked")
+        List<T> dest = (List<T>) in.readObject();
+        return dest;
     }
 
 }
