6da8314af5085c35abf4d5224553f18be3979b2b
yeachiu <32219751+yeachiu@users.noreply.github.com>
2019-04-26 18:14:24 +0800

    Create ActivityFormUpdateDTO.java

---
+++ src/main/java/cn/licoy/wdog/core/dto/app/activity/ActivityFormUpdateDTO.java
@@ -0,0 +1,13 @@
+package cn.licoy.wdog.core.dto.app.activity;
+
+import com.alibaba.fastjson.JSON;
+import lombok.Data;
+
+
+@Data
+public class ActivityFormUpdateDTO {
+
+    private String actiId;
+
+    private String rules;
+}
